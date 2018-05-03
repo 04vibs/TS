48b1393286be80f16db8d7ad83d8fe19fc8d8e73
04vibs <04vibs@gmail.com>
2018-05-03 11:20:35 +0530

    exclude changes

--- app/functions/function.ts
+++ app/functions/function.ts
@@ -1,31 +1,29 @@
-// /**
-//  * Created by aayusharora on 5/1/18.
-//  */
-//
-//
+
 // function hello(n: string, r: number, s:string): string{
 //     return "2";
 // }
 //
 // hello("2",2, "3");
-//
-//
+
+
 // let pizza = function(count: number): string {
 //     return "3";
 // }
-//
-//
+
+
 // function myCar(car: {name: string, type: string}): boolean{
 //    return true;
 // }
-//
+
 // let obj1= {
 //     name: "Maruti",
 //     type: "X-10",
 //     color: "string"
 // };
-//
+
 // myCar(obj1);
+
+
 let pizza: {make: string, model: string};
 
 pizza = {
